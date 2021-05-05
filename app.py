59e20c94377899b849f780022c268ce90fe47da6
elizabeth-16 <83184371+elizabeth-16@users.noreply.github.com>
2021-05-05 22:50:39 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import sklearn
+import joblib
+model = joblib.load('/content/Yelp_model')
+st.title('Yelp Classifier')
+ip = st.text_input("Enter the message")
+op = model.predict([ip])
+if st.button('Predict'):
+  st.title(op[0])
